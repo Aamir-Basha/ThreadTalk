9db38ae8d9d1ee772621b20b828c89bcb04b07c2
amer-basha <a.mosa-basha@tu-braunschweig.de>
2024-09-21 22:59:10 +0200

    WIN

--- IPC/src/server/src/main.c
+++ IPC/src/server/src/main.c
@@ -1,21 +1,47 @@
-#include "client.h"
+#include "../include/ring_buffer.h"
+#include "../include/server.h"
 
-int main(void)
+#include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+
+/**
+ * @brief signal_handler: be sure to exit the programm
+ * @param sig: signal
+ */
+void signal_handler(int sig)
+{
+    //close all threads and the socket by receiving a signal
+    //close_connection();
+    printf("\n");
+    exit(sig);
+}
+
+/**
+ * @brief initialize_signal_handler: handle signals by users
+ */
+void initialize_signal_handler(void)
+{
+    signal(SIGINT,  signal_handler);
+    signal(3,       signal_handler);
+    signal(SIGILL,  signal_handler);
+    signal(SIGABRT, signal_handler);
+    signal(SIGFPE,  signal_handler);
+    signal(SIGSEGV, signal_handler); 
+    signal(SIGTERM, signal_handler);
+
+}
+
+int main()
 {
 
-    // Task .1
-    connect_to_server();
+    int server_socket = initialize_server();
+    printf("server successfully initialized\n");
 
-    // Task .2
-    handshake();
+    initialize_signal_handler();
 
-    // Task .6
-    start_reader_thread();
+    ringbuffer_initialize();
+    accept_connections(server_socket);
 
-    // Task .3 to .5
-    send_message();
-
-    //this part should never be reached
-    close_connection();
-    return EXIT_FAILURE;
+    return 0;
 }
